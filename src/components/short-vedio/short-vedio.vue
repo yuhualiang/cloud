bd60ec7fd4b5fc11a729c520644128af6a7f8272
mengxiang <1547719290@qq.com>
2020-04-01 11:22:58 +0800

    将axios请求数据换成数组

--- src/components/short-vedio/short-vedio.vue
+++ src/components/short-vedio/short-vedio.vue
@@ -16,7 +16,7 @@ import Sort from '../sort/sort'
 import VedioList from '../vedio-list/vedio-list'
 import Scroll from '../../common/scroll/scroll'
 import Loading from '../../common/loading/loading'
-import axios from 'axios'
+// import axios from 'axios'
 
 export default {
   components: { Sort, VedioList, Scroll, Loading },
@@ -34,9 +34,133 @@ export default {
       // console.log(id)
     },
     _getDataList() {
-      axios.get('./data.json').then((res) => {
-        this.listData = res.data
-      })
+      setTimeout(() => {
+        this.listData = [
+          {
+            Title: 'Tcenter产品演讲',
+            id: '29362',
+            online_number: 51,
+            comment: '0',
+            coverpicture: 'http://juxian.juyun.tv/images/videoqg.jpg',
+            live: 'live/2020030929362.html',
+            live_state: 0,
+            state: '等待直播',
+            name: '泰德网聚',
+            photo: 'http://img.juyun.tv/console/2020/02/25/1582613827.jpg'
+          },
+          {
+            Title: 'test',
+            id: '29341',
+            online_number: 42,
+            comment: '11',
+            coverpicture: 'http://juxian.juyun.tv/images/videoqg.jpg',
+            live: 'live/2020030829341.html',
+            live_state: 0,
+            state: '等待直播',
+            name: '泰德网聚',
+            photo: 'http://img.juyun.tv/console/2020/02/25/1582613827.jpg'
+          },
+          {
+            Title: '报名重复提交测试',
+            id: '29238',
+            online_number: 11,
+            comment: '3',
+            coverpicture: 'http://juxian.juyun.tv/images/videoqg.jpg',
+            live: 'live/2020030329238.html',
+            live_state: 0,
+            state: '等待直播',
+            name: '泰德网聚',
+            photo: 'http://img.juyun.tv/console/2020/02/25/1582613827.jpg'
+          },
+          {
+            Title: '围观CMS',
+            id: '29178',
+            online_number: 30006,
+            comment: '5',
+            coverpicture: 'http://juxian.juyun.tv/images/videoqg.jpg',
+            live: 'live/2020030229178.html',
+            live_state: 0,
+            state: '等待直播',
+            name: '泰德网聚',
+            photo: 'http://img.juyun.tv/console/2020/02/25/1582613827.jpg'
+          },
+          {
+            Title: '刷围观测试',
+            id: '29177',
+            online_number: 30006,
+            comment: '0',
+            coverpicture: 'http://juxian.juyun.tv/images/videoqg.jpg',
+            live: 'live/2020030229177.html',
+            live_state: 0,
+            state: '等待直播',
+            name: '泰德网聚',
+            photo: 'http://img.juyun.tv/console/2020/02/25/1582613827.jpg'
+          },
+          {
+            Title: '线上直播课（六年级）',
+            id: '29129',
+            online_number: 1,
+            comment: '1',
+            coverpicture: 'https://img.juyun.tv/user/1582878354.jpg',
+            live: 'live/2020022829129.html',
+            live_state: 0,
+            state: '等待直播',
+            name: '泰德网聚',
+            photo: 'http://img.juyun.tv/console/2020/02/25/1582613827.jpg'
+          },
+          {
+            Title: '线上直播课（五年级）',
+            id: '29128',
+            online_number: 0,
+            comment: '0',
+            coverpicture: 'https://img.juyun.tv/user/1582878284.jpg',
+            live: 'live/2020022829128.html',
+            live_state: 0,
+            state: '等待直播',
+            name: '泰德网聚',
+            photo: 'http://img.juyun.tv/console/2020/02/25/1582613827.jpg'
+          },
+          {
+            Title: '线上直播课（四年级）',
+            id: '29127',
+            online_number: 1,
+            comment: '0',
+            coverpicture: 'https://img.juyun.tv/user/1582878242.jpg',
+            live: 'live/2020022829127.html',
+            live_state: 0,
+            state: '等待直播',
+            name: '泰德网聚',
+            photo: 'http://img.juyun.tv/console/2020/02/25/1582613827.jpg'
+          },
+          {
+            Title: '线上直播课（三年级）',
+            id: '29126',
+            online_number: 0,
+            comment: '0',
+            coverpicture: 'https://img.juyun.tv/user/1582878204.jpg',
+            live: 'live/2020022829126.html',
+            live_state: 0,
+            state: '等待直播',
+            name: '泰德网聚',
+            photo: 'http://img.juyun.tv/console/2020/02/25/1582613827.jpg'
+          },
+          {
+            Title: '线上直播课（二年级）',
+            id: '29125',
+            online_number: 0,
+            comment: '0',
+            coverpicture: 'https://img.juyun.tv/user/1582878161.jpg',
+            live: 'live/2020022829125.html',
+            live_state: 0,
+            state: '等待直播',
+            name: '泰德网聚',
+            photo: 'http://img.juyun.tv/console/2020/02/25/1582613827.jpg'
+          }
+        ]
+      }, 1000)
+      // axios.get('./data.json').then(res => {
+      //   this.listData = res.data
+      // })
     }
   }
 }
@@ -45,11 +169,13 @@ export default {
 .short-vedio {
   position: fixed;
   width: 100%;
-  top: 2.4rem /* 120/50 */
+  top: 2.4rem; /* 120/50 */
   bottom: 0;
+
   .content {
     height: 100%;
     overflow: hidden;
+
     .loading-container {
       position: absolute;
       width: 100%;
